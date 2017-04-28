737dc23cc3edd82f252ff88b0780f42a6d4f5e8e
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-04-28 11:50:16 +0700

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,59 +8,10 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
+                    You are logged in!
                 </div>
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody class="connectedSortable">
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </table>
             </div>
         </div>
     </div>
 </div>
-<style>
-    tr.ui-sortable-helper{background-color: #fff;display: table!important;border: 1px solid #ddd;}
-</style>
-<script>
-    $(document).ready(function() {
-        $( "tbody.connectedSortable" ).sortable({
-            connectWith: ".connectedSortable",
-            items: "> tr:not(:first)",
-            helper:"clone",
-            zIndex: 999990,
-            start: function(event, ui){
-                //ui.item.startHtml = ui.item.html();
-            },
-            stop: function(event, ui){ 
-                //ui.item.html(ui.item.startHtml);
-            }
-        })
-        .disableSelection()
-    });
-</script>
 @endsection
